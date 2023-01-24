2c0a57fdd17f9d697eccb11c914c2fa0e68e47ab
Santiago <santiso@gmail.com>
2023-01-24 12:07:15 -0300

    connection

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
  module.exports = {
 	"development": {
-		"username": process.env.DB_USER,
-		"password": process.env.DB_PASS,
-		"database": process.env.DB_NAME,
-		"host": process.env.DB_HOST,
-		"port": process.env.DB_PORT,
+		"username": process.env.DB_USER || 'root',
+		"password": process.env.DB_PASS || null,
+		"database": process.env.DB_NAME || "undergames_db",
+		"host": process.env.DB_HOST || 'localhost',
+		"port": process.env.DB_PORT || 3306,
 		"dialect": "mysql",
 		"logging": false,
 		"operatorsAliases": 0
