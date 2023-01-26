dc3006b1eeb9a396f5b66b0c4f55580cb7630384
Santiago <santiso@gmail.com>
2023-01-26 16:33:53 -0300

    cambios en index.js match con config.js db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,16 +1,16 @@
-/* require('dotenv').config();
+require('dotenv').config();
 
  module.exports = {
 	"development": {
-		"port": process.env.PORT,
-		"username": process.env.DB_USER || 'root',
-		"password": process.env.DB_PASS || '',
-		"database": process.env.DB_NAME || 'undergames_db',
-		"host": process.env.DB_HOST || 'localhost',
-		"dbport": process.env.DB_PORT || 3306,
-		"dialect": "mysql",
-		"logging": false,
-		"operatorsAliases": 0
+		PORT: process.env.PORT,
+		DB_USER: process.env.DB_USER || 'root',
+		DB_PASS: process.env.DB_PASS || '',
+		DB_NAME: process.env.DB_NAME || 'undergames_db',
+		DB_HOST: process.env.DB_HOST || 'localhost',
+		DB_PORT: process.env.DB_PORT || 3306,
+		dialect: "mysql",
+		logging: false,
+		operatorsAliases: 0
 	},
 	
 	"test": {
@@ -37,7 +37,7 @@
 	}
 	
 }  
-  */
+  
  
 /* require('dotenv').config();
 module.exports = 
@@ -65,11 +65,14 @@ module.exports =
 	  "dialect": "mysql"
 	}
   }  */
-
+/* 
 export const PORT = process.env.PORT || 3000
 
+
 export const DB_HOST = process.env.DB_HOST || 'localhost'
+
 export const DB_USER = process.env.DB_USER || 'root'
+
 export const DB_PASSWORD = process.env.DB_PASSWORD || null
 export const DB_NAME = process.env.DB_NAME || 'undergames_db'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_PORT = process.env.DB_PORT || 3306 */
