35d5fdc62e58a226fbecd7a1a612589dc6dcddf8
Santiago <santiso@gmail.com>
2023-01-26 16:06:11 -0300

    changes config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/* require('dotenv').config();
 
  module.exports = {
 	"development": {
@@ -37,7 +37,7 @@ require('dotenv').config();
 	}
 	
 }  
- 
+  */
  
 /* require('dotenv').config();
 module.exports = 
@@ -65,3 +65,11 @@ module.exports =
 	  "dialect": "mysql"
 	}
   }  */
+
+export const PORT = process.env.PORT || 3000
+
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || null
+export const DB_NAME = process.env.DB_NAME || 'undergames_db'
+export const DB_PORT = process.env.DB_PORT || 3306
