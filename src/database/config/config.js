9a867224a0108f6f69893f4df6e47ee6a4701fb6
Santiago <santiso@gmail.com>
2023-01-30 17:19:48 -0300

    cambios

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
  module.exports = {
 	"development": {
-		PORT: process.env.PORT,
+
 		DB_USER: process.env.DB_USER,
 		DB_PASS: process.env.DB_PASS,
 		DB_NAME: process.env.DB_NAME,
