7efc46db1cb9f83bc6e0df44ebb176780eeed7e1
Santiago <santiso@gmail.com>
2023-01-30 13:09:18 -0300

    prueba clevercloud

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
  module.exports = {
 	"development": {
 		PORT: process.env.PORT,
-		DB_USER: process.env.DB_USER || 'root',
-		DB_PASS: process.env.DB_PASS || '',
-		DB_NAME: process.env.DB_NAME || 'undergames_db',
-		DB_HOST: process.env.DB_HOST || 'localhost',
-		DB_PORT: process.env.DB_PORT || 3306,
+		DB_USER: process.env.DB_USER,
+		DB_PASS: process.env.DB_PASS,
+		DB_NAME: process.env.DB_NAME,
+		DB_HOST: process.env.DB_HOST,
+		DB_PORT: process.env.DB_PORT,
 		dialect: "mysql",
 		logging: false,
 		operatorsAliases: 0
