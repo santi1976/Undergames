bdd3b3d62fc0615da45f4b24c819f8647d85aa3e
Santiago <santiso@gmail.com>
2023-01-31 13:01:41 -0300

    arreglos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 	"production": {
 
 		DB_USER: process.env.DB_USER || 'root',
-		DB_PASS: process.env.DB_PASS || '',
+		DB_PASS: process.env.DB_PASS || 'password',
 		DB_NAME: process.env.DB_NAME || 'undergames_db',
 		DB_HOST: process.env.DB_HOST || '127.0.0.1',
 		DB_PORT: process.env.DB_PORT || 3306,
