ba772f49839e126b31ee8e6098ac848a67c852ce
Santiago <santiso@gmail.com>
2023-02-01 17:01:03 -0300

    cambios en ejs por error en tos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 
  module.exports = {
 	"production": {
-
+		PORT: process.env.PORT || 3008,
 		DB_USER: process.env.DB_USER || 'root',
-		DB_PASS: process.env.DB_PASS || 'password',
+		DB_PASS: process.env.DB_PASS ||  null,
 		DB_NAME: process.env.DB_NAME || 'undergames_db',
 		DB_HOST: process.env.DB_HOST || '127.0.0.1',
-		DB_PORT: process.env.DB_PORT || 3306,
+		DB_PORT: process.env.DB_PORT ||  3306,
 		dialect: "mysql",
 		logging: false,
 		operatorsAliases: 0
